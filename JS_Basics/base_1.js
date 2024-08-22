e0f1d600da9085bccbc03d34ecc9adac31ebc345
ekamleen <ekamleenm@gmail.com>
2024-08-22 15:48:27 -0700

    js

---
+++ JS_Basics/base_1.js
@@ -0,0 +1,14 @@
+// Throughout the whole program 
+var myName = 'ekam';
+myName = 9;
+
+// Will only be used within  the scope where it is used
+let ourName = 'xyz';
+
+// can't be redefined but can be changed/updated 
+ourName = 'sdfgd'
+
+// this should never change.
+const pi = 3.14; 
+
+
