f13561c8064ccdce6e286dd471991da2b804e626
ekamleen <ekamleenm@gmail.com>
2024-08-23 21:21:23 -0700

    objects

---
+++ IntermediateJS/Objects.js
@@ -0,0 +1,15 @@
+// Time Stamp : 45 : 00 
+let product = {
+  company : 'mango',
+  item_name : 'cotton T shirt',
+  price: 550,
+};
+
+console.log(typeof product);
+console.log(product);
+console.log(product.price)
+
+product.color = "Green";
+console.log(product);
+product.price = 345;
+console.log(product);
