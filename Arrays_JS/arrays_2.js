7996a9b94482a757bd916e7f0944fda3fdcd55eb
ekamleen <ekamleenm@gmail.com>
2024-09-03 15:23:17 -0700

    arrays

--- Arrays_JS/arrays_2.js
+++ Arrays_JS/arrays_2.js
@@ -1,3 +1,4 @@
+
 // Different methods of Array 
 
 let arr = [1,2,3,4,5,6,7,8,9];
@@ -11,4 +12,24 @@ console.log(arr);
 // 1 23 45 9 is the way its sorted 23 is alphabetially less than 9.
 arr.sort();
 
+//  for-each loop 
 
+let foods = ['bread','jam','milk','butter'];
+
+foods.forEach(function(food){
+  console.log(food);
+});
+
+// Return doesn't work in forEach loop be careful 
+
+// Array advance functions 
+// .map .filter 
+
+let arr2  = [3,6,7,9,0,5];
+
+let newArr = arr2.map(function(item) {
+  console.log(item);
+  return item*item;
+});
+
+console.log(newArr);
