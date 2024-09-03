5f01d31a19c9378ca5bb21ee75a41901417732a0
ekamleen <ekamleenm@gmail.com>
2024-09-03 14:17:08 -0700

    Objects done

---
+++ IntermediateJS/objects_2.js
@@ -0,0 +1,9 @@
+// # AutoBoxers : Primitive Variables can call some methods 
+// string.length gives the length of the string 
+// let num = 76 ; 
+// num.toFixed(4) -> converts into string. 
+// We have number string class that have these related methods 
+// when we call any method on primitives autoboxing converts primitive 
+// into repective object and then method is called. 
+
+// 
