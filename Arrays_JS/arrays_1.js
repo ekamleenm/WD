73bf5610dc0b032c90042f6e7a1fb5ace8d7714b
ekamleen <ekamleenm@gmail.com>
2024-09-03 14:53:26 -0700

    arrays

---
+++ Arrays_JS/arrays_1.js
@@ -0,0 +1,43 @@
+// Same kind of Objects 
+let student1 = {
+  firstName : "ekam1",
+  lastName : "maan1",
+  age: 22,
+  rollNo : 11,
+};
+
+let student2 = {
+  firstName : "ekam2",
+  lastName : "maan2",
+  age: 23,
+  rollNo : 12,
+};
+
+let student3 = {
+  firstName : "ekam3",
+  lastName : "maan3",
+  age: 24,
+  rollNo : 13,
+};
+
+
+let prime1 = 2;
+let prime2 = 3;
+let prime3 = 5;
+
+// We will use Array 
+
+let primeNumber = [2,3,5];
+
+// We can store diff data types in same array 
+
+let multiArray = [1, "ekam", 2.144, null, {likes: "21 Million"}];
+
+console.log(multiArray[0])
+console.log(multiArray[1])
+console.log(multiArray[2])
+console.log(multiArray[3])
+
+
+// Array varaible stores address and the memory used at that address depends upon the content of 
+// the array, but the variable size remains same usually 8 Bytes 
---
+++ Arrays_JS/arrays_2.js
@@ -0,0 +1,14 @@
+// Different methods of Array 
+
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(Array.isArray(arr));  
+
+console.log(arr);
+arr.push(56);
+console.log(arr);
+
+// arr.sort() sort the original array dont make a copy.  This sorts like alphabetically 
+// 1 23 45 9 is the way its sorted 23 is alphabetially less than 9.
+arr.sort();
+
+
