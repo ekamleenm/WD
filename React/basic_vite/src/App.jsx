c0cf5d76b4a5a3bfa78603e374c0718f3d968b28
ekamleen <ekamleenm@gmail.com>
2024-10-03 23:43:56 -0700

    R

---
+++ React/basic_vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import Youtube from './Youtube'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite React App</h1>
+      <Youtube />
+    </>
+    
+
+  )
+}
+
+export default App
---
+++ React/basic_vite/src/Youtube.jsx
@@ -0,0 +1,13 @@
+function Youtube(){
+
+  return (
+    <>
+      <h1>This is Youtube</h1>
+      <h2>This is 2nd element that i returned</h2>
+    </>
+    
+  )
+
+}
+
+export default Youtube
